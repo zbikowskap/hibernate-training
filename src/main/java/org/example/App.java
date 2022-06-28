6beaa4e08f93196edaab5d47746d9c3b2ce18fce
Adam Pusio <adam.pusio@outlook.com>
2022-06-28 18:45:22 +0200

    WIP - hibernate

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,8 +1,15 @@
 package org.example;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
 public class App
 {
     public static void main( String[] args ) {
+        HibernateFactory hibernateFactory = new HibernateFactory();
+        SessionFactory sessionFactory = hibernateFactory.getSessionFactory();
+        Session session = sessionFactory.openSession();
 
+        session.close();
     }
 }
